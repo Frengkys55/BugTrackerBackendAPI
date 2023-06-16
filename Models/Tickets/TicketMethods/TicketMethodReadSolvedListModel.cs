677d07f0b2a7114cdc1d4bf84e73c1e4c284808a
frengkys55 <frengkysinaga@windowslive.com>
2023-06-16 17:33:42 +0900

    Implement method to get all solved tickets

---
+++ Models/Tickets/TicketMethods/TicketMethodReadSolvedListModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BugTrackerBackendAPI.Models
+{
+    public partial class Ticket
+    {
+        public async Task<ICollection<ShortTicket>> GetAllSolvedTicketList(string accesstoken, string connectionString)
+        {
+            Collection<ShortTicket> list = new Collection<ShortTicket>();
+
+            Data.DbHelper.GenericRead<ShortTicket> dbRead = new Data.DbHelper.GenericRead<ShortTicket>();
+
+            string query = "SELECT * FROM GetSolvedTicketShort ('" + accesstoken.ToString() + "')";
+            try
+            {
+                return await dbRead.Read(query, connectionString);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
