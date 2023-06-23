6c320431cc782ea7b1aa855a7bf58f50f47e929b
Frengkys55 <frengkysinaga@windowslive.com>
2023-06-23 14:37:16 +0900

    Fixing method for adding new commen

--- Models/Comments/CommentMethods/CommentMethodAddModel.cs
+++ Models/Comments/CommentMethods/CommentMethodAddModel.cs
@@ -9,11 +9,14 @@ namespace BugTrackerBackendAPI.Models
             Data.DbHelper.Procedure.Executor executor = new Data.DbHelper.Procedure.Executor(connectionString);
             string query = "AddComment";
 
-            List<string> ignoreProperty = new List<string>();
-            ignoreProperty.Add(nameof(comment.Guid));
-            ignoreProperty.Add(nameof(comment.DateCreated));
-            ignoreProperty.Add(nameof(comment.HasAttachment));
-            ignoreProperty.Add(nameof(comment._hasAttachment));
+            List<string> ignoreProperty = new List<string>
+            {
+                nameof(comment.Guid),
+                nameof(comment.DateCreated),
+                nameof(comment.HasAttachment),
+                nameof(comment.TicketName),
+                nameof(comment._hasAttachment)
+            };
 
             List<KeyValuePair<string, dynamic>> additionalProperties = new List<KeyValuePair<string, dynamic>>();
             additionalProperties.Add(new KeyValuePair<string, dynamic>("accesstoken", accesstoken));
